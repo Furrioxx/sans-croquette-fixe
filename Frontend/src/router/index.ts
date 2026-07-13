3925d9216d77cd610c78e1b694a427287e9d26be
Furrioxx <114649898+Furrioxx@users.noreply.github.com>
2026-07-13 14:23:40 +0200

    [feature/global-enhance] fix scroll on page change

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -10,6 +10,15 @@ const FALLBACK_APP_NAME = "Sans Croquettes Fixes - Association d'adoption de cha
 const router = createRouter({
   history: createWebHistory(),
   routes: routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    if (to.hash) {
+      return { el: to.hash }
+    }
+    return { top: 0 }
+  },
 })
 
 router.beforeEach(async (to, _from, next) => {
