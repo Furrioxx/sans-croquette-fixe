543624c81e3f3ea5f2771ba76da9fec9044a5024
Furrioxx <114649898+Furrioxx@users.noreply.github.com>
2026-07-18 14:44:00 +0200

    [feat/volunteer] fix catsheet modal

--- Frontend/src/components/Modals/CatSheetModal.vue
+++ Frontend/src/components/Modals/CatSheetModal.vue
@@ -162,11 +162,6 @@ const saving = ref(false)
 const save = async () => {
   saving.value = true
 
-  if (firstInvalidStep) {
-    activeStep.value = firstInvalidStep
-    return
-  }
-
   try {
     const { keptIds, pendingFiles } = imagesPanel.value!.getState()
 
