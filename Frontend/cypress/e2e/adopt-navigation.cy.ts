9c2d20a144926a02df4c1bfa5fcf9140fbce0089
m <max.cl.dev@gmail.com>
2026-07-22 17:50:37 +0200

    test: align adopt navigation with cat sheet query

--- Frontend/cypress/e2e/adopt-navigation.cy.ts
+++ Frontend/cypress/e2e/adopt-navigation.cy.ts
@@ -1,71 +1,52 @@
 describe('adopt navigation', () => {
   it('navigates from the adopt list to a cat sheet detail', () => {
-    cy.intercept('GET', '**/cat-sheets?*', {
-      statusCode: 200,
-      body: {
-        data: [
-          {
-            documentId: 'sheet-1',
-            isDuo: false,
-            description: null,
-            tarification: null,
-            images: [],
-            cats: [
-              {
-                name: 'Félix',
-                birthDate: '2022-01-01',
-                gender: 'male',
-                catStatus: 'en_refuge',
-                cat_moods: [],
-                vaccinated: true,
-                sterilized: true,
-                identified: true,
-                decontaminate: true,
-                catFriendly: 'yes',
-                dogFriendly: 'yes',
-                childFriendly: 'yes',
-              },
-            ],
-          },
-        ],
-        meta: { pagination: { page: 1, pageSize: 9, total: 1 } },
-      },
-    }).as('getCatSheets')
-
-    cy.intercept('GET', '**/cat-sheets/sheet-1?*', {
-      statusCode: 200,
-      body: {
-        data: {
-          documentId: 'sheet-1',
-          isDuo: false,
-          description: null,
-          tarification: null,
-          images: [],
-          cats: [
-            {
-              name: 'Félix',
-              birthDate: '2022-01-01',
-              gender: 'male',
-              catStatus: 'en_refuge',
-              cat_moods: [],
-              vaccinated: true,
-              sterilized: true,
-              identified: true,
-              decontaminate: true,
-              catFriendly: 'yes',
-              dogFriendly: 'yes',
-              childFriendly: 'yes',
-            },
-          ],
+    const catSheet = {
+      documentId: 'sheet-1',
+      isDuo: false,
+      description: null,
+      tarification: null,
+      images: [],
+      cats: [
+        {
+          name: 'Felix',
+          birthDate: '2022-01-01',
+          gender: 'male',
+          catStatus: 'en_refuge',
+          cat_moods: [],
+          vaccinated: true,
+          sterilized: true,
+          identified: true,
+          decontaminate: true,
+          catFriendly: 'yes',
+          dogFriendly: 'yes',
+          childFriendly: 'yes',
         },
-      },
-    }).as('getCatSheetDetail')
+      ],
+    }
+
+    cy.intercept('GET', '**/cat-sheets?*', (req) => {
+      const isDetailRequest = req.url.includes('documentId')
+
+      req.reply({
+        statusCode: 200,
+        body: {
+          data: [catSheet],
+          meta: {
+            pagination: {
+              page: 1,
+              pageSize: isDetailRequest ? 1 : 9,
+              total: 1,
+            },
+          },
+        },
+      })
+    }).as('getCatSheets')
 
     cy.visit('/adopt')
     cy.wait('@getCatSheets')
     cy.get('[data-cy=cat-sheet-card]').first().find('[data-cy=cat-sheet-see-more]').click()
 
-    cy.wait('@getCatSheetDetail')
+    cy.wait('@getCatSheets')
     cy.location('pathname').should('include', '/adopt/sheet-1')
   })
 })
